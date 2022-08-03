67dba8e2d355bc1c5c77c1f53f1e0b3b9ca14947
santialvesz <santialvesdsc@gmail.com>
2022-08-03 09:25:00 -0700

    Some fixes

--- simple_shell.c
+++ simple_shell.c
@@ -1,38 +1,40 @@
 #include "main.h"
-/**
-*
-*
-*/
+
+
 int main(__attribute__((unused)) int ac,  __attribute__((unused)) char **av, char **env)
 {
-	char *buff = NULL, *token, *args[1024];
+	char *buffer = NULL, *token;
 	int status = 0;
-	size_t str = 0, len = 0;
+	char *args[1024];
+	size_t str, len = 0;
+	int interactive = 1;
+	int child;
 
-	while (1)
+	while (interactive)
 	{
-		if (isatty(0))
-		if (getline(&buff, &len, stdin) == -1)
+
+		if (getline(&buffer, &len, stdin) == -1)
 			break;
-		token = strtok(buff, " \t\n\r");
-		if (!_strcmp(token, "exit"))
+		token = strtok(buffer, " \t\n"); /* aca tokenizas hasta un tab o enter*/
+		if (!strcmp(token, "exit")) /* usas strcmp pa comparar lo que te pasaron */
 		{
-			free(buff);
+			free(buffer);
 			return (str);
 		}
 		for (str = 0; str < 1024 && token != NULL; str++)
 		{
 			args[str] = token;
-			token = strtok(NULL, " \t\n\r");
+			token = strtok(NULL, " \t\n");
 		}
 		args[str] = NULL;
 		if (!args[0])
 		{
 			free(args[0]);
-			free(buff);
+			free(buffer);
 			return (0);
 		}
-		if (fork() == 0)
+		child = fork();
+		if (child == 0)
 		{
 
 			if (execve(args[0], args, env) == -1)
@@ -44,6 +46,6 @@ int main(__attribute__((unused)) int ac,  __attribute__((unused)) char **av, cha
 		else
 			wait(&status);
 	}
-	free(buff);
+	free(buffer);
 	return (0);
 }
