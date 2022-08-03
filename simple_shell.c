65089355957782f89fc63277a29afb187cacd88d
santialvesz <santialvesdsc@gmail.com>
2022-08-03 09:11:13 -0700

    Remaking the shell

--- simple_shell.c
+++ simple_shell.c
@@ -2,31 +2,23 @@
 /**
 *
 *
-*
-*
 */
-
 int main(__attribute__((unused)) int ac,  __attribute__((unused)) char **av, char **env)
 {
-	char *buffer = NULL, *token;
+	char *buff = NULL, *token, *args[1024];
 	int status = 0;
-	char *args[1024];
-	size_t str, len = 0;
-	int interactive = 1;
-	int child;
+	size_t str = 0, len = 0;
 
-	while (interactive)
+	while (1)
 	{
-		interactive = isatty(0); /* checkea modo interactivo */
-		/* printf("# "); aqui deberia ir cisfun$ */
-
-		if (getline(&buffer, &len, stdin) == -1) /* Esto es el control D */
+		if (isatty(0))
+		if (getline(&buff, &len, stdin) == -1)
 			break;
-		token = strtok(buffer, " \t\n\r"); /* aca tokenizas hasta un tab o enter*/
-		if (!strcmp(token, "exit")) /* Funcion exit,  usas strcmp pa comparar lo que te pasaron */
+		token = strtok(buff, " \t\n\r");
+		if (!_strcmp(token, "exit"))
 		{
-			free(buffer);
-			return(0);	
+			free(buff);
+			return (str);
 		}
 		for (str = 0; str < 1024 && token != NULL; str++)
 		{
@@ -37,11 +29,10 @@ int main(__attribute__((unused)) int ac,  __attribute__((unused)) char **av, cha
 		if (!args[0])
 		{
 			free(args[0]);
-			free(buffer);
+			free(buff);
 			return (0);
 		}
-		child = fork();
-		if (child == 0)
+		if (fork() == 0)
 		{
 
 			if (execve(args[0], args, env) == -1)
@@ -53,6 +44,6 @@ int main(__attribute__((unused)) int ac,  __attribute__((unused)) char **av, cha
 		else
 			wait(&status);
 	}
-	free(buffer);
+	free(buff);
 	return (0);
 }
